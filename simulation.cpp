01d2f085b963864414c4ceffc4853834de2c6145
Joeri de Wagt <joeri-1996@live.nl>
2020-05-17 21:27:30 +0200

    small cleaning

--- simulation.cpp
+++ simulation.cpp
@@ -84,7 +84,6 @@ void Simulation::tick()
         if(s.infected())
         {
             s.set_sick();
-            //s.set_immune();
             if (s.sick() == 0) {
                 numberInfected--;
             }
